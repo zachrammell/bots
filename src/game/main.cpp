cd85cb33c2cf90b4938cf6464a2839144f1ea94c
Zardium <zrammell13@gmail.com>
2020-08-16 00:53:31 -0700

    Minimum buildable

---
+++ src/game/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Game started.\n";
+  return 0;
+}
---
+++ src/test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <VNgine/engine.h>
+
+int main()
+{
+  std::cout << "Tests started." << std::endl;
+  return 0;
+}
