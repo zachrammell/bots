6906bc66c7dae8aa9bb1034c6277d779ad6f50ad
Zardium <zrammell13@gmail.com>
2020-08-16 02:49:42 -0700

    libraries work

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <Windows.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+namespace
+{
+  glm::vec4 clear_color = { 1.0f, 0.75f, 0.33f, 1.0f };
+}
 
 int main()
 {
   std::cout << "Game started.\n";
+
+  glfwInit();
+  GLFWwindow* window = glfwCreateWindow(800, 400, "Robogirls Game", nullptr, nullptr);
+  glfwShowWindow(window);
+
+  glfwMakeContextCurrent(window);
+
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+
+    glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+
   return 0;
 }
